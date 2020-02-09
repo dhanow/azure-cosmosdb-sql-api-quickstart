33b9af9948d5a27598c6283a1dae6d322e450dee
dhanow <dhanow@yahoo.com>
2020-02-08 21:53:21 -0600

    latest

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CosmosdbSqlApiQuickstart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
